bf2fcb708b9349a9b60658545bbc4aea7684761c
ThantzinTun007 <tzt.thantzintun2022@gmail.com>
2024-11-16 09:17:55 +0630

    change category

--- src/app/components/app-activities/app-activities.component.ts
+++ src/app/components/app-activities/app-activities.component.ts
@@ -7,22 +7,10 @@ import { appsName } from './apps-data';
   styleUrl: './app-activities.component.css',
 })
 export class AppActivitiesComponent {
-  activeItem: string | null = null;
-  // apps = appsName;
+  apps = appsName;
 
-  setActive(item: string): void {
-    this.activeItem = this.activeItem === item ? null : item;
-  }
+  selectedCategory = 'Favorites';
 
-  selectedCategory = 'Favorites'; // Default tab
-  apps = [
-    { imgPath: 'assets/icons/manage-product.png', name: 'Manage Product Master Data', category: 'Favorites' },
-    { imgPath: 'assets/icons/maintain-pirs.png', name: 'Maintain PIRs', category: 'Favorites' },
-    { imgPath: 'assets/icons/schedule-mrp.png', name: 'Schedule MRP Runs', category: 'Recent Activities' },
-    // Add more apps here
-  ];
-  
-  // Function to switch categories
   selectCategory(category: string) {
     this.selectedCategory = category;
   }
--- src/app/components/app-activities/apps-data.ts
+++ src/app/components/app-activities/apps-data.ts
@@ -1,6 +1,17 @@
 export const appsName = [
   {
-    imgPath: 'assets/logo/SAP_logo.png',
-    name: 'logo logo',
+    imgPath: 'assets/icons/manage-product.png',
+    name: 'Manage Product Master Data',
+    category: 'Favorites',
+  },
+  {
+    imgPath: 'assets/icons/maintain-pirs.png',
+    name: 'Maintain PIRs',
+    category: 'Favorites',
+  },
+  {
+    imgPath: 'assets/icons/schedule-mrp.png',
+    name: 'Schedule MRP Runs',
+    category: 'Recent Activities',
   },
 ];
