3bddb8d8049dde10a7d6bdc76536bb8a0ad4afc3
ThantzinTun007 <tzt.thantzintun2022@gmail.com>
2024-11-16 11:32:10 +0630

    end of app and activities

--- src/app/components/app-activities/apps-data.ts
+++ src/app/components/app-activities/apps-data.ts
@@ -1,17 +1,167 @@
 export const appsName = [
   {
-    imgPath: 'assets/icons/manage-product.png',
-    name: 'Manage Product Master Data',
+    imgPath: 'assets/logo/icon1.png',
+    name: 'Manage Product Master ',
     category: 'Favorites',
   },
   {
-    imgPath: 'assets/icons/maintain-pirs.png',
-    name: 'Maintain PIRs',
+    imgPath: 'assets/logo/icon2.png',
+    name: 'Manage Product Master ',
     category: 'Favorites',
   },
   {
-    imgPath: 'assets/icons/schedule-mrp.png',
+    imgPath: 'assets/logo/icon3.png',
+    name: 'Manage Product Master ',
+    category: 'Favorites',
+  },
+  {
+    imgPath: 'assets/logo/icon4.png',
+    name: 'Manage Product Master ',
+    category: 'Favorites',
+  },
+  {
+    imgPath: 'assets/logo/icon5.png',
+    name: 'Manage Product Master ',
+    category: 'Favorites',
+  },
+  {
+    imgPath: 'assets/logo/icon6.png',
+    name: 'Manage Product Master ',
+    category: 'Favorites',
+  },
+  {
+    imgPath: 'assets/logo/icon2.png',
+    name: 'Manage Product Master ',
+    category: 'Favorites',
+  },
+  {
+    imgPath: 'assets/logo/icon1.png',
+    name: 'Manage Product Master ',
+    category: 'Favorites',
+  },
+  {
+    imgPath: 'assets/logo/icon6.png',
+    name: 'Manage Product Master ',
+    category: 'Favorites',
+  },
+  {
+    imgPath: 'assets/logo/icon4.png',
+    name: 'Manage Product Master ',
+    category: 'Favorites',
+  },
+  {
+    imgPath: 'assets/logo/icon5.png',
+    name: 'Manage Product Master ',
+    category: 'Favorites',
+  },
+  {
+    imgPath: 'assets/logo/icon3.png',
+    name: 'Manage Product Master ',
+    category: 'Favorites',
+  },
+  {
+    imgPath: 'assets/logo/icon1.png',
+    name: 'Manage Product Master ',
+    category: 'Favorites',
+  },
+  {
+    imgPath: 'assets/logo/icon3.png',
+    name: 'Manage Product Master ',
+    category: 'Favorites',
+  },
+  {
+    imgPath: 'assets/logo/icon6.png',
+    name: 'Manage Product Master ',
+    category: 'Favorites',
+  },
+  {
+    imgPath: 'assets/logo/icon2.png',
+    name: 'Manage Product Master ',
+    category: 'Favorites',
+  },
+
+  // for recent
+  {
+    imgPath: 'assets/logo/icon4.png',
     name: 'Schedule MRP Runs',
     category: 'Recent Activities',
+  },
+  {
+    imgPath: 'assets/logo/icon5.png',
+    name: 'Schedule MRP Runs',
+    category: 'Recent Activities',
+  },
+  {
+    imgPath: 'assets/logo/icon2.png',
+    name: 'Schedule MRP Runs',
+    category: 'Recent Activities',
+  },
+  {
+    imgPath: 'assets/logo/icon1.png',
+    name: 'Schedule MRP Runs',
+    category: 'Recent Activities',
+  },
+  {
+    imgPath: 'assets/logo/icon3.png',
+    name: 'Schedule MRP Runs',
+    category: 'Recent Activities',
+  },
+  {
+    imgPath: 'assets/logo/icon5.png',
+    name: 'Schedule MRP Runs',
+    category: 'Recent Activities',
+  },
+  {
+    imgPath: 'assets/logo/icon6.png',
+    name: 'Schedule MRP Runs',
+    category: 'Recent Activities',
+  },
+  {
+    imgPath: 'assets/logo/icon5.png',
+    name: 'Schedule MRP Runs',
+    category: 'Recent Activities',
+  },
+
+
+  // for fre
+  {
+    imgPath: 'assets/logo/icon1.png',
+    name: 'Schedule MRP Runs',
+    category: 'Frequently Used',
+  },
+  {
+    imgPath: 'assets/logo/icon2.png',
+    name: 'Schedule MRP Runs',
+    category: 'Frequently Used',
+  },
+  {
+    imgPath: 'assets/logo/icon6.png',
+    name: 'Schedule MRP Runs',
+    category: 'Frequently Used',
+  },
+  {
+    imgPath: 'assets/logo/icon5.png',
+    name: 'Schedule MRP Runs',
+    category: 'Frequently Used',
+  },
+  {
+    imgPath: 'assets/logo/icon4.png',
+    name: 'Schedule MRP Runs',
+    category: 'Frequently Used',
+  },
+  {
+    imgPath: 'assets/logo/icon3.png',
+    name: 'Schedule MRP Runs',
+    category: 'Frequently Used',
+  },
+  {
+    imgPath: 'assets/logo/icon2.png',
+    name: 'Schedule MRP Runs',
+    category: 'Frequently Used',
+  },
+  {
+    imgPath: 'assets/logo/icon1.png',
+    name: 'Schedule MRP Runs',
+    category: 'Frequently Used',
   },
 ];
