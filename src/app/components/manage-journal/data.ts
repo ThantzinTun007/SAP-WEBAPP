9a874aca76db668091184c80e76b75f59abc1d10
ThantzinTun007 <tzt.thantzintun2022@gmail.com>
2024-11-24 09:54:42 +0630

    adapt filter

---
+++ src/app/components/manage-journal/data.ts
@@ -0,0 +1,47 @@
+export const document = [
+  {
+    text: 'Journal Entry Data :',
+    type: 'text',
+    icon: 'fa-regular fa-clone',
+  },
+  {
+    text: 'Posting Date :',
+    type: 'date',
+    icon: '',
+  },
+  {
+    text: 'Fiscal Year :',
+    type: 'text',
+    icon: 'fa-regular fa-clone',
+  },
+  {
+    text: 'Facial Period :',
+    type: 'text',
+    icon: 'fa-regular fa-clone',
+  },
+  {
+    text: 'Journal Entry Created By :',
+    type: 'text',
+    icon: 'fa-regular fa-clone',
+  },
+  {
+    text: 'Journal Entry Type :',
+    type: 'text',
+    icon: 'fa-regular fa-clone',
+  },
+  {
+    text: 'Journal Entry Category :',
+    type: 'text',
+    icon: 'fa-regular fa-clone',
+  },
+  {
+    text: 'Absolute Exchange Rate',
+    type: 'text',
+    icon: 'fa-regular fa-clone',
+  },
+  {
+    text: 'Additional Crcy 1',
+    type: 'text',
+    icon: 'fa-regular fa-clone',
+  },
+];
--- src/app/components/manage-journal/manage-journal.component.ts
+++ src/app/components/manage-journal/manage-journal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { document } from './data';
 
 @Component({
   selector: 'app-manage-journal',
@@ -8,4 +9,6 @@ import { Component } from '@angular/core';
 export class ManageJournalComponent {
   options: string[] = ['All', 'Some', ' Other'];
   defOption: string = 'All';
+
+  documentData = document;
 }
